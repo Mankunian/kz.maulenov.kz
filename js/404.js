9541a4a6d1705f1108aa0aa8309efefb610428d8
d.aidarkhan <reddevils1878>
2018-10-31 00:35:02 +0600

    1

---
+++ js/404.js
@@ -0,0 +1,9 @@
+// 404 Error page
+//Responsive: Large desktop (1800px), Standard, Mobile (568px)
+
+
+// iPhone pull addressbar (Optional)
+/mobile/i.test(navigator.userAgent) && !window.location.hash && setTimeout(function () {
+    window.scrollTo(0, 1);
+}, 1000);
+
