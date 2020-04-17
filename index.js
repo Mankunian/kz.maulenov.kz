08e9ad72c1d4c020277929b01a6355da78effbd8
d.aidarkhan <reddevils1878>
2020-04-17 16:03:40 +0600

    design

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
-// angular.module('mainApp', ['pascalprecht.translate'])
-// 	app
-// 	.controller('navCtrl', function($scope, $window, $location) {
-// 		$scope.langList = [
-// 			{id: 1, name: 'Русский'},
-// 			{id: 2, name: 'Қазақша'}
-// 		]
-
-// 		$scope.update = function(lang){
-// 			console.log(lang)
-// 			if (lang.id === 2) {
-// 				$window.$location.href = '/ideas.html'
-// 			}
-// 		}
-// 	})
 var app = angular.module('mainApp', ['pascalprecht.translate']);
 
 app.config(function	($translateProvider){
 	$translateProvider.translations('ru', {
-		"TITLE": 'Наука должна быть весёлая, увлекательная и простая. Таковыми же должны быть и ученые'
+		"TITLE1": 'Наука должна быть весёлая,',
+		"TITLE2": 'увлекательная и простая.',
+		"TITLE3": 'Таковыми же должны',
+		"TITLE4": 'быть и ученые',
+		"AUTHOR": 'Петр Капица',
+		"PRIVET": 'Приветственное слово',
+		"BIO": 'Биография',
+		"MEDAL": 'Награды и поощрения',
+		"TESTIMONIALS": 'Цитаты',
+		"VIDEO": 'Видео',
+		"NETWORK": 'Общественная деятельность',
+		"CERTIFICATE": 'Сертификаты',
+		"PHOTO": 'Фото',
+		"IDEAS": 'Иное творчество',
+		"RESEARCH": 'Научные труды',
+		"NEWSPAPER": 'Научные статьи',
+		"MEDIA": 'Публикации в СМИ',
+		"CONTACTS": 'Контактные данные',
+		"ADDRESS_TITLE": 'Мы находимся',
+		"ADDRESS_CITY": 'г. Тараз',
+		"ADDRESS": 'ул. Т.Акбозов, 27',
+		"MAIL": 'Свяжитесь с нами по почте',
+		"SOCIAL": 'Подписывайтесь в соц.сетях'
 	})
 
 	$translateProvider.translations('kz', {
-		"TITLE": 'Қол жазуды ермек ет, жатпа бекер, бұл көңілсіз дүниеден көп жыл отер.'
+		"TITLE1": ' "Қол жазуды ермек ет, жатпа бекер,',
+		"TITLE2": 'Бұл көңілсіз дүниеден көп жыл өтер.',
+		"TITLE3": 'Өзі қысқа, өзі асау, тентек өмір,',
+		"TITLE4": 'Арттағыға бір белгі қойса нетер?"',
+		"AUTHOR": 'Абай',
+		"PRIVET": 'Сәлемсөз',
+		"BIO": 'Өмірбаян',
+		"MEDAL": 'Марапаттар',
+		"TESTIMONIALS": 'Цитаталар',
+		"VIDEO": 'Видеолар',
+		"NETWORK": 'Қоғамдық қызмет',
+		"CERTIFICATE": 'Сертификаттар мен дипломдар',
+		"PHOTO": 'Фотолар',
+		"IDEAS": 'Өзге шығармашылық',
+		"RESEARCH": 'Ғылыми еңбектер',
+		"NEWSPAPER": 'Ғылыми мақалалар',
+		"MEDIA": 'БАҚ-тағы жариялымдар',
+		"CONTACTS": 'Кері байланыс',
+		"ADDRESS_TITLE": 'Біздің мекен-жайымыз',
+		"ADDRESS_CITY": 'Тараз қаласы',
+		"ADDRESS": 'Т.Акбозов көшесі, 27',
+		"MAIL": 'Пошта арқылы хабарласыңыз',
+		"SOCIAL": 'Желілер арқылы хабарласыңыз'
 	})
 
 	$translateProvider.preferredLanguage('ru');
 })
 
 app.controller('navCtrl', function($scope, $translate) {
+	$scope.ruLang = $translate.use('ru').$$state.value
+	console.log($scope.ruLang)
 
 	$scope.langList = [
 					{id: 1, name: 'Русский', code: 'ru'},
@@ -35,7 +66,11 @@ app.controller('navCtrl', function($scope, $translate) {
 				]
 
 	$scope.changeLang = function(key) {
+		if (key == 'ru') {
+			$scope.ruLang = key;
+		}
 		console.log(key)
-		$translate.use(key.code)
+		$translate.use(key)
+		// $translate.use(key.code)
 	}
 })
